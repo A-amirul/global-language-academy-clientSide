fbfe2a7aab759d1bfded0c8a3deb9b192bbfa12d
A-amirul <amirulislamamir412@gmail.com>
2023-06-10 14:57:07 +0600

    create a custom hook useInstructors

--- src/pages/Home/Home/Instructors/Instructors.jsx
+++ src/pages/Home/Home/Instructors/Instructors.jsx
@@ -1,8 +1,10 @@
+import useInstructors from "../../../../hooks/useInstructors";
 
 const Instructors = () => {
+	const [instructors] = useInstructors();
 	return (
 		<div>
-			<h2>Instructor</h2>
+			<h2>Instructor:{instructors?.length}</h2>
 		</div>
 	);
 };
