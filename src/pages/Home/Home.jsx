25f183be18a8cc972c5a6f33e778137a685a4cd5
A-amirul <amirulislamamir412@gmail.com>
2023-06-14 02:45:42 +0600

    About section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import useTitle from "../../../useTitle";
+import About from "../../components/About";
 import Banner from "./Home/Banner/Banner";
 import PopularClasses from "./Home/PopularClasses/PopularClasses";
 import PopularInstructors from "./Home/PopularInstructors/PopularInstructors";
@@ -7,27 +7,13 @@ import PopularInstructors from "./Home/PopularInstructors/PopularInstructors";
 const Home = () => {
 
 	useTitle("Home");
-
-			const [isDarkMode, setIsDarkMode] = useState(false);
-
-	const handleToggle = () => {
-				setIsDarkMode(!isDarkMode);
-	};
-
-			return (
-			<div className={`bg-${isDarkMode ? 'gray-900' : 'gray-100'} min-h-screen text-${isDarkMode ? 'white' : 'gray-800'}`}>
-				<Banner></Banner>
-				<PopularClasses></PopularClasses>
-				<PopularInstructors></PopularInstructors>
-
-				<button
-					className={`fixed bottom-4 right-4 rounded-full p-2 bg-${isDarkMode ? 'yellow-400' : 'gray-600'} text-white`}
-					onClick={handleToggle}
-				>
-					{isDarkMode ? 'Light Mode' : 'Dark Mode'}
-				</button>
-			</div>
-
+	return (
+		<div>
+			<Banner></Banner>
+			<PopularClasses></PopularClasses>
+			<PopularInstructors></PopularInstructors>
+			<About></About>
+		</div>
 	);
 };
 
